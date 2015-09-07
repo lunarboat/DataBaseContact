9d296dc53b6cab0ceab8b5dee4dc676d8b79b5f3
lunarboat <lunarboat@outlook.com>
2015-09-07 18:54:08 +0800

    Initial Commit

---
+++ DataBaseContact/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DataBaseContact
+//
+//  Created by lunarboat on 15/9/7.
+//  Copyright (c) 2015年 lunarboat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DataBaseContact/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DataBaseContact
+//
+//  Created by lunarboat on 15/9/7.
+//  Copyright (c) 2015年 lunarboat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
